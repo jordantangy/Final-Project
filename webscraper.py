7c187e6b5d116bf6e21e725673144abf70a00731
jordantangy@gmail.com <jordantangy@gmail.com>
2021-03-10 01:23:09 +0200

    update

--- webscraper.py
+++ webscraper.py
@@ -13,20 +13,31 @@ truncat = truncat.to_numpy()
 
 webpage = "https://www.imdb.com/find?s=tt&q="
 plots = []
-for i in range(3):
+for i in range(6):
+    # take the movie name from database
     movie_name = truncat[i][2]
+    #turn it into a string
     query = str(movie_name)
+    # remove spaces
     query = query.replace(" ", "")
+    # add components to create a working web address
     temp = webpage+query
     link = temp + "&ref_=nv_sr_sm"
+    # declare chrome driver and get the url
     driver = webdriver.Chrome(executable_path='chromedriver.exe')
     driver.get(url=link)
+    # click on the first movie on the page
     driver.find_element_by_css_selector("""#main > div > div.findSection > table > tbody > tr:nth-child(1) > td.result_text > a""").click()
+    # get the url of the current web page
     url = driver.current_url
+    # get the page and parse it into html
     page = requests.get(url)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    trun = soup.find("div", class_="summary_text").extract()
-    plots.append(trun)
+    soup = BeautifulSoup(url.content, 'html.parser')
+    # exctract the plot and insert it into the plots array
+    plot = soup.find("div", class_="summary_text").extract()
+    plots.append(plot)
+    # close the page
     driver.close()
 print(plots)
+
 
